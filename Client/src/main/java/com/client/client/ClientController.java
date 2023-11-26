e7d9bab0aef7593ba850c84080388abc81258d4b
Edoardo Oglietti <59146921+00-uno-00@users.noreply.github.com>
2023-11-26 20:24:21 +0100

    Working Popup yet to be completed

--- Client/src/main/java/com/client/client/ClientController.java
+++ Client/src/main/java/com/client/client/ClientController.java
@@ -1,10 +1,17 @@
 package com.client.client;
 
+import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -20,12 +27,16 @@ public class ClientController implements Initializable {
     private Button changeAccount;
 
     @FXML
-    private AnchorPane buttonAnchor;
+    private VBox window;
 
     @FXML
     private ListView<String> contactsList;
 
     private List<Contact> contacts = new ArrayList<>();
+
+    private Stage stage = new Stage();
+    private Scene scene;
+    private Parent root;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -34,13 +45,29 @@ public class ClientController implements Initializable {
         loadContacts();
     }
 
+    public void openPopup(MouseEvent event) {
+        try {
+            root = javafx.fxml.FXMLLoader.load(getClass().getResource("email.fxml"));
+            scene = new Scene(root);
+            stage.setTitle("New Email");
+            stage.setScene(scene);
+            stage.show();
+
+
+            // disable main window while popup is open
+            window.setDisable(true);
+            // TODO: add listener to enable main window when popup is closed
+        } catch (Exception e) {
+            System.out.println("Error opening popup");
+        }
+    }
 
     /**
      * Load contacts from a CSV file
      */
     public void loadContacts() {
         try {
-            File contactsFile = new File("Client\\Client\\src\\main\\resources\\com\\client\\client\\contacts.csv");
+            File contactsFile = new File(getClass().getResource("contacts.csv").getFile());//use getClass().getResource("contacts.csv").getFile() or aneurysm
             Scanner scanner = new Scanner(contactsFile);
 
             while (scanner.hasNextLine()) {
---
+++ Client/src/main/java/com/client/client/emailController.java
@@ -0,0 +1,18 @@
+package com.client.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class emailController implements Initializable {
+    @FXML
+    private TextField recipient;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
