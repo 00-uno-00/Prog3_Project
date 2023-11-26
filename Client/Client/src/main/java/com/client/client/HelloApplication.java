4a46dd2a99b5a8df8afeb5c0585fbaf99f04476b
Edoardo Oglietti <59146921+00-uno-00@users.noreply.github.com>
2023-11-26 16:06:46 +0100

    Client update

--- Client/Client/src/main/java/com/client/client/HelloApplication.java
+++ Client/Client/src/main/java/com/client/client/HelloApplication.java
@@ -12,9 +12,10 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("client.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
+
         stage.setScene(scene);
         stage.show();
     }
--- Client/Client/src/main/java/com/client/client/TestController.java
+++ Client/Client/src/main/java/com/client/client/TestController.java
@@ -3,40 +3,60 @@ package com.client.client;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import org.kordamp.bootstrapfx.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 public class TestController implements Initializable {
 
     @FXML
-    private Button Reply;
+    private Button changeAccount;
 
     @FXML
-    private Button ReplyAll;
+    private AnchorPane buttonAnchor;
 
     @FXML
-    private Button Forward;
+    private ListView<String> contactsList;
 
-    @FXML
-    private Button Delete;
+    private List<Contact> contacts = new ArrayList<>();
 
 
-    @FXML
-    private Button ChangeAccount;
-
-    /**
-     * Set Bootstrap CSS for the client
-     * @param url
-     * @param resourceBundle
-     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        Reply.getStyleClass().setAll("btn","btn-default");
-        ReplyAll.getStyleClass().setAll("btn","btn-default");
-        Forward.getStyleClass().setAll("btn","btn-primary");
-        Delete.getStyleClass().setAll("btn","btn-danger");
-        ChangeAccount.getStyleClass().setAll("btn","btn-default");
+        changeAccount.getStyleClass().setAll("btn","btn-default");
+
+        loadContacts();
     }
 
+
+    /**
+     * Load contacts from a CSV file
+     */
+    public void loadContacts() {
+        try {
+            File contactsFile = new File("Client\\Client\\src\\main\\resources\\com\\client\\client\\contacts.csv");
+            Scanner scanner = new Scanner(contactsFile);
+
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] contactData = line.split(",");
+                contacts.add(new Contact(contactData[0], contactData[1], contactData[2]));
+            }
+        } catch (FileNotFoundException e){
+            System.out.println("File not found");
+        }
+
+        for (Contact contact : contacts) {
+            contactsList.getItems().add(contact.getName() + " " + contact.getSurname());
+        }
+    }
 }
