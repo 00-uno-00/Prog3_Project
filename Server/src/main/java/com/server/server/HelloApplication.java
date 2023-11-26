2c6d1d344dde96ad7be6f4fe2e645ddf47ede397
davide.vitturini <davide.vitturini@edu.unito.it>
2023-11-26 18:19:11 +0100

    Trying to fix weird error with server folder

--- Server/src/main/java/com/server/server/HelloApplication.java
+++ Server/src/main/java/com/server/server/HelloApplication.java
@@ -1,4 +1,4 @@
-package com.example.server;
+package com.server.server;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
--- Server/src/main/java/com/server/server/HelloController.java
+++ Server/src/main/java/com/server/server/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.server;
+package com.server.server;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
