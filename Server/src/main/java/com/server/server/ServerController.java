fa8f49e6fae07f71a352cca9f630f3bc9071bb0c
davide.vitturini <davide.vitturini@edu.unito.it>
2023-11-26 18:21:54 +0100

    Trying to fix weird error with server folder

--- Server/src/main/java/com/server/server/ServerController.java
+++ Server/src/main/java/com/server/server/ServerController.java
@@ -3,7 +3,7 @@ package com.server.server;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ServerController {
     @FXML
     private Label welcomeText;
 
