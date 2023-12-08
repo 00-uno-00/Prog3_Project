e584d7512d198a46a4b85c8d4ea0cdcbf2cbf180
davide.vitturini <davide.vitturini@edu.unito.it>
2023-12-08 17:42:24 +0100

    Added client icon

--- Client/src/main/java/com/client/client/ClientApplication.java
+++ Client/src/main/java/com/client/client/ClientApplication.java
@@ -3,13 +3,21 @@ package com.client.client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ClientApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        // Load the server icon
+        Image icon = new Image(Objects.requireNonNull(ClientApplication.class.getResource("icons/mail.png")).toExternalForm());
+        // Set the icon for the stage
+        stage.getIcons().add(icon);
+
         FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
 
